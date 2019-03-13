b68d73b13a5e9fd726397912c21162e5ab9fea14
Perry <19218868+perrykaufman@users.noreply.github.com>
2019-03-13 17:23:56 -0500

    completed timer functionality

--- src/scripts/simple-timer.js
+++ src/scripts/simple-timer.js
@@ -1,24 +1,45 @@
 import EventMixin from './event-mixin.js'
+import { on } from 'cluster';
 
 const INTERVAL = 1000
 
 class SimpleTimer {
-  constructor(minutes, seconds) {
+  constructor({minutes = 0, seconds = 0}) {
     this.startTime = {minutes, seconds}
-    this.seconds = seconds
     this._state = {
       minutes: 0,
       seconds: 0,
     }
+    this._interval = null
   }
   _tick() {
-    //TODO
+    if (this.minutes == 0 && this.seconds == 0) {
+      this.stop()
+    } else if (this.seconds == 0) {
+      this.minutes--
+      this.seconds = 59
+    } else {
+      this.seconds--
+    }
+    this.dispatch('tick', {minutes: this.minutes, seconds: this.seconds})
   }
   start() {
-    //TODO
+    this.minutes = this.startTime.minutes
+    this.seconds = this.startTime.seconds
+    this._interval = setInterval(() => this._tick(), INTERVAL)
+    this.dispatch('start')
   }
-  reset() {
-    //TODO
+  reset({minutes, seconds = 0}) {
+    if (minutes) {
+      this.startTime.minutes = minutes
+      this.startTime.seconds = seconds
+    }
+    this.stop()
+  }
+  stop() {
+    clearInterval(this._interval)
+    this._interval = null
+    this.dispatch('stop')
   }
   get minutes() {
     return this._state.minutes
@@ -39,6 +60,10 @@ class SimpleTimer {
     }
     this._state.seconds = Math.floor(value)
     this.dispatch('seconds-change', this._state.seconds)
+  }
+  get isActive() {
+    if (this._interval) return true
+    return false
   }
 }
 
