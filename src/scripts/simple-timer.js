1c0631318c6f72713d6d402e35fc7327e3c112cb
Perry <19218868+perrykaufman@users.noreply.github.com>
2019-03-14 21:46:29 -0500

    improved interface and refactored

--- src/scripts/simple-timer.js
+++ src/scripts/simple-timer.js
@@ -3,62 +3,76 @@ import EventMixin from './event-mixin.js'
 const INTERVAL = 1000
 
 class SimpleTimer {
-  constructor({minutes = 0, seconds = 0}) {
-    this.startTime = {minutes, seconds}
-    this._state = {
-      minutes: 0,
-      seconds: 0,
-    }
+  constructor(startTime) {
+    this._state = {}
+    this._startTime = startTime
     this._interval = null
   }
   _tick() {
-    if (this.minutes == 0 && this.seconds == 0) {
+    if (this._minutes == 0 && this._seconds == 0) {
       this.stop()
-    } else if (this.seconds == 0) {
-      this.minutes--
-      this.seconds = 59
+    } else if (this._seconds == 0) {
+      this._minutes--
+      this._seconds = 59
     } else {
-      this.seconds--
+      this._seconds--
     }
-    this.dispatch('tick', {minutes: this.minutes, seconds: this.seconds})
+    this.dispatch('tick', {minutes: this._minutes, seconds: this._seconds})
   }
-  start() {
-    this.minutes = this.startTime.minutes
-    this.seconds = this.startTime.seconds
+  start(startTime) {
+    if (startTime) this._startTime = startTime
+    this._minutes = this._startTime.minutes
+    this._seconds = this._startTime.seconds
     this._interval = setInterval(() => this._tick(), INTERVAL)
     this.dispatch('start')
   }
-  reset({minutes, seconds = 0}) {
-    if (minutes) {
-      this.startTime.minutes = minutes
-      this.startTime.seconds = seconds
-    }
+  reset(startTime) {
     this.stop()
+    if (startTime) {
+      this._startTime = startTime
+    }
   }
   stop() {
-    clearInterval(this._interval)
+    if (this._interval) {
+      clearInterval(this._interval)
+    }
     this._interval = null
     this.dispatch('stop')
   }
   get minutes() {
+    return this.isActive ? this._minutes : this._startTime.minutes
+  }
+  get _minutes() {
     return this._state.minutes
   }
-  set minutes(value) {
-    if (typeof value != 'number') {
-      throw new TypeError('minutes must be a number')
-    }
+  set _minutes(value) {
     this._state.minutes = Math.floor(value)
     this.dispatch('minutes-change', this._state.minutes)
   }
   get seconds() {
+    return this.isActive ? this._seconds : this._startTime.seconds
+  }
+  get _seconds() {
     return this._state.seconds
   }
-  set seconds(value) {
-    if (typeof minutes != 'number') {
-      throw new TypeError('seconds must be a number')
-    }
+  set _seconds(value) {
     this._state.seconds = Math.floor(value)
     this.dispatch('seconds-change', this._state.seconds)
+  }
+  get _startTime() {
+    return Object.assign({}, this._state.startTime)
+  }
+  set _startTime({minutes = 0, seconds = 0} = {}) {
+    if (typeof minutes != 'number') {
+      throw new TypeError('minutes must be a number')
+    }
+    if (typeof seconds != 'number') {
+      throw new TypeError('seconds must be a number')
+    }
+    this._state.startTime = {
+      minutes: minutes > 0 ? Math.floor(minutes) : 0,
+      seconds: seconds > 0 ? Math.floor(seconds) : 0
+    }
   }
   get isActive() {
     if (this._interval) return true
@@ -66,6 +80,6 @@ class SimpleTimer {
   }
 }
 
-Object.assign(SimpleTimer, EventMixin)
+Object.assign(SimpleTimer.prototype, EventMixin)
 
 export default SimpleTimer
