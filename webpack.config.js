9b39d350e9589252ba4c6780d6c952082895151b
Perry <19218868+perrykaufman@users.noreply.github.com>
2019-03-15 06:29:54 -0500

    added polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: [['@babel/preset-env', {useBuiltIns: 'usage'}]]
           }
         }
       }
