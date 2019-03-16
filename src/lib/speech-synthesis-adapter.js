77847d205d308692fd9e4218babb4b81fa9d188b
Perry <19218868+perrykaufman@users.noreply.github.com>
2019-03-16 02:09:20 -0500

    adapter for speechSynthesis

---
+++ src/lib/speech-synthesis-adapter.js
@@ -0,0 +1,28 @@
+/* Caller API
+ * call(string)
+ * getVoices() returns array of voices {name, index}
+ * setVoice()
+ */
+
+class SpeechSynthesisAdapter {
+  constructor() {
+    this._voice = null
+  }
+  call(text) {
+    const utterance = new SpeechSynthesisUtterance('text')
+    if (this._voice) utterance.voice = this._voice
+    speechSynthesis.speak(utterance)
+  }
+  getVoices() {
+    return speechSynthesis.getVoices()
+      .filter(({lang}) => {
+        return lang.toLowerCase() == 'en-us'
+      })
+      .map(({name}, index) => {
+        return {name, index}
+      })
+  }
+  setVoice(name) {
+    this._voice = speechSynthesis.getVoices().find((el) => el.name === name)
+  }
+}
