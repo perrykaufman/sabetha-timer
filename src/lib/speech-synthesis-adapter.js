2bf2bbb5fe894e28080ebb6e325b35f285a2d3ba
Perry <19218868+perrykaufman@users.noreply.github.com>
2019-03-19 15:47:57 -0500

    applied eslint/prettier

--- src/lib/speech-synthesis-adapter.js
+++ src/lib/speech-synthesis-adapter.js
@@ -6,26 +6,30 @@
 
 class SpeechSynthesisAdapter {
   constructor() {
-    this._voice = null
+    this._voice = null;
   }
+
   call(text) {
-    const utterance = new SpeechSynthesisUtterance(text)
-    if (this._voice) utterance.voice = this._voice
-    utterance.rate = 1.25
-    speechSynthesis.speak(utterance)
+    const utterance = new SpeechSynthesisUtterance(text);
+    if (this._voice) utterance.voice = this._voice;
+    utterance.rate = 1.25;
+    speechSynthesis.speak(utterance);
   }
-  getVoices() {
-    return speechSynthesis.getVoices()
-      .filter(({lang}) => {
-        return lang.toLowerCase() == 'en-us'
+
+  static getVoices() {
+    return speechSynthesis
+      .getVoices()
+      .filter(({ lang }) => {
+        return lang.toLowerCase() === "en-us";
       })
-      .map(({name}, index) => {
-        return {name, index}
-      })
+      .map(({ name }, index) => {
+        return { name, index };
+      });
   }
+
   setVoice(name) {
-    this._voice = speechSynthesis.getVoices().find((el) => el.name === name)
+    this._voice = speechSynthesis.getVoices().find(el => el.name === name);
   }
 }
 
-export default SpeechSynthesisAdapter
+export default SpeechSynthesisAdapter;
